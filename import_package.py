81e53b70c409104757073380f779d70c6ff0ba54
Koushani Biswas <koushani-biswas@uiowa.edu>
2025-11-13 11:26:31 -0600

    Create import_package.py
    This script is purely for importing and loading the needed packages for our analysis

---
+++ import_package.py
@@ -0,0 +1,10 @@
+
+#installing neo package
+!pip install neo
+#importing LFP
+#neuralynx module helps us with reading our continuous data
+from neo.io import NeuralynxIO
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from google.colab import drive
